865595a0232936e05a4d6553005f628b1ab3566e
abdul23lm <abdul23lm@gmail.com>
2021-04-29 08:21:59 +0700

    Endpoint setup for user

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\MeController;
+use Illuminate\Support\Facades\{Route, Auth};
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Auth::loginUsingId(1);
+Route::middleware('auth:sanctum')->group(function () {
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::get('me', [MeController::class, '__invoke']);
 });
